7f4de7627defe0c7020cebbfb4f597f098a2fce7
Shaun <shaunm28@icloud.com>
2020-05-03 20:11:44 +0200

    Servrlets and JSPs

---
+++ LiquorStoreApp/src/main/java/sample/SelectLiquorServlet.java
@@ -0,0 +1,31 @@
+package sample;
+
+import model.LiquorType;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(
+        name = "selectliquorservlet",
+        urlPatterns = "/SelectLiquor"
+)
+
+public class SelectLiquorServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String liquorType = request.getParameter("Type");
+        LiquorService liquorService = new LiquorService();
+        LiquorType l = LiquorType.valueOf(liquorType);
+        List liquorBrands = liquorService.getAvailableBrands(l);
+        request.setAttribute("brands", liquorBrands);
+        RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+        view.forward(request, response);
+    }
+}
